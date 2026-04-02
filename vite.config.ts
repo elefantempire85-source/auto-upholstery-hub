b1537df348870d0210f0a275f7c138cedd22a4f0
elefantempire85-source <elefantempire85@gmail.com>
2026-04-02 12:55:50 -0700

    add cname + custom domain setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/auto-upholstery-hub/",   // 👈 ADD THIS
+  base: "/",   // 👈 ADD THIS
 
   server: {
     host: "::",

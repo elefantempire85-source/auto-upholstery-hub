12389b3c52d1197758e0b96ad899f473d001c909
elefantempire85-source <elefantempire85@gmail.com>
2026-04-02 12:36:48 -0700

    chore: add gitignore and remove dist/node_modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
